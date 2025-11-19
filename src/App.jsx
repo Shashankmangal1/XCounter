77d6fd5f35ffe6915cadad73567866156b20aa31
Shashank Mangal <127585673+Shashankmangal1@users.noreply.github.com>
2025-11-19 12:50:23 +0530

    Completed Xclasscomp

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import React, { Component } from "react";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="container">
-      <h1>Count: {count}</h1>
+    this.state = {
+      count: 0,
+    };
+  }
 
-      <div className="btn-container">
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Count: {this.state.count}</h1>
+
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
